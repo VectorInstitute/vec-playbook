f23ccd9b810b77a144f997ba5284ae1f888ae418
kohankhaki <fkohankh8@gmail.com>
2025-08-13 16:50:47 -0400

    updated the launch command to use config file. added sweep launch.

--- starters/llm_fine_tuning/text_classification/launch.sh
+++ starters/llm_fine_tuning/text_classification/launch.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
+# Single job launcher using experiment config file
 
 TRAINER="starters.llm_fine_tuning.text_classification.trainer:HFTextClassificationTrainer"
+CONFIG_FILE="$(dirname "$0")/config.yaml"
+
+echo "Launching single job with config: $CONFIG_FILE"
 
 vec-tool submit "$TRAINER" \
-  --job-dir "./vec_jobs" \
-  --nodes 1 \
-  --gpus-per-node 1 \
-  --gpu-type a40 \
-  --time "1:00:00" \
-  --name "hf-textcls" \
-  --kw "num_epochs=2 batch_size=16 lr=5e-5 save_steps=200 eval_steps=200"
+    --experiment-config "$CONFIG_FILE"
