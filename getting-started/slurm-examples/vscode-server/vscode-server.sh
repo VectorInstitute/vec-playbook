1fdfc0c074cf95ee53973f064c7e3e049f486e6c
Mark Coatsworth <mark.coatsworth@vectorinstitute.ai>
2025-10-07 14:50:12 -0400

    Temporary workaround for wrong shell in code-server module

--- getting-started/slurm-examples/vscode-server/vscode-server.sh
+++ getting-started/slurm-examples/vscode-server/vscode-server.sh
@@ -11,4 +11,6 @@ module load code-server
 
 # Start the VS Code server
 # I've arbitrarily used port 9001 in this example. Any high number port should work.
-code-server --bind-addr $(hostname --fqdn):9001 --auth none
+# BUG: The module code is running under /bin/sh which is wrong. Let's manually call /bin/bash until this gets fixed
+#code-server --bind-addr $(hostname --fqdn):9001 --auth none
+/bin/bash /cvmfs/soft.computecanada.ca/easybuild/software/2023/x86-64-v3/Core/code-server/4.101.2/bin/code-server --bind-addr $(hostname --fqdn):9001 --auth none
