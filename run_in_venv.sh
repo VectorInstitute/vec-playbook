3500556b1a2385fd6a73c7c6d05c1c4508a15eaa
Jacob-Junqi Tian <jacob@banana.abay.cf>
2025-10-29 02:08:05 -0400

    Cleaned up run_in_venv.sh

--- run_in_venv.sh
+++ run_in_venv.sh
@@ -2,5 +2,4 @@
 unset VIRTUAL_ENV
 unset VIRTUAL_ENV_PROMPT
 source $SCRATCH/uv-venvs/vllm-serving/bin/activate
-export UV_PROJECT_ENVIRONMENT=/scratch/ssd004/scratch/jacobtian/uv-venvs/vllm-serving
 $@
